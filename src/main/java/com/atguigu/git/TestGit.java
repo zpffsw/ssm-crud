559b7346f54c897c012b142e618e469b7f02029a
zp <2443965847@qq.com>
2022-03-28 20:41:58 +0800

    master four commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -10,6 +10,7 @@ public class TestGit {
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
         System.out.println("Hello Git4!");
+        System.out.println("master test!");
 
 
     }
