c900320a4efbc990b51b8833a5558bc8b2404369
zp <2443965847@qq.com>
2022-03-28 20:28:23 +0800

    second commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -7,5 +7,6 @@ package com.atguigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
